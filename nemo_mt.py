fded8557ad0aa84abc396cd698b552e14f52da11
khanhdv <khanhdv@sonek.vn>
2023-08-04 17:03:26 +0700

    init nemo demo app

---
+++ nemo_mt.py
@@ -0,0 +1,12 @@
+from nemo.collections.nlp.models import MTEncDecModel
+
+# To get the list of pre-trained models
+MTEncDecModel.list_available_models()
+
+# Download and load the a pre-trained to translate from English to Spanish
+model = MTEncDecModel.from_pretrained("nmt_zh_en_transformer6x6")
+
+# Translate a sentence or list of sentences
+translations = model.translate(["这是我年轻时候住的房子。"], source_lang="zh", target_lang="en")
+
+print (translations)
